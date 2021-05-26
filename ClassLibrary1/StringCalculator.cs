a00a831e83a70c90e27bc492629dc5528c6a068e
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-05-26 09:34:17 +0530

    step 1

--- ClassLibrary1/StringCalculator.cs
+++ ClassLibrary1/StringCalculator.cs
@@ -1,6 +1,21 @@
-﻿namespace ClassLibrary1
+﻿using System.Linq;
+
+namespace ClassLibrary1
 {
     public class StringCalculator
     {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            if (!numbers.Contains(','))
+            {
+                return int.Parse(numbers);
+            }
+            string [] nums = numbers.Split(',');
+            return int.Parse(nums[0]) + int.Parse(nums[1]);
+        }
     }
 }
--- TestProject1/StringCalculatorTests.cs
+++ TestProject1/StringCalculatorTests.cs
@@ -1,13 +1,36 @@
 using Xunit;
+using ClassLibrary1;
 
 namespace TestProject1
 {
     public class StringCalculatorTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly StringCalculator _stringCalculator;
 
+        public StringCalculatorTests()
+        {
+            _stringCalculator = new StringCalculator();
+        }
+
+        [Fact]
+        public void CanEmptyStringReturnZero()
+        {
+            int sum = _stringCalculator.Add(string.Empty);
+            Assert.Equal(0, sum);
+        }
+
+        [Fact]
+        public void CanReturnOneNum()
+        {
+            int sum = _stringCalculator.Add("2");
+            Assert.Equal(2, sum);
+        }
+
+        [Fact]
+        public void CanSumTwoNums()
+        {
+            int sum = _stringCalculator.Add("2,4");
+            Assert.Equal(6, sum);
         }
     }
 }
